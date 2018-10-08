643c3307ee30187c0d12c0fb355f3f248d6d63b2
Antoine Galland <toanan31@gmail.com>
2018-10-08 17:47:00 +0200

    Refactoring and using PnPClientContext

---
+++ SPOtLight/MainWindow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SPOtLight;
+using Microsoft.SharePoint.Client;
+
+namespace SPOtLight
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Mainwindows()
+        {
+            InitializeComponent();
+        }
+
+        private void StorePW(object sender, RoutedEventArgs e)
+        {
+            var repo = new PasswordRepository();
+            var tryConnect = new SPOtLightMenu();
+
+            if (!string.IsNullOrEmpty(TBUN.Text))
+            {  
+                repo.SaveCred(PBPW.SecurePassword, TBUN.Text);
+                this.Hide();
+                
+            }
+            CredentialManagement.Credential cred = repo.GetCred();
+            if (cred.Exists())
+            {
+                this.Hide();
+                new SPOtLightMenu().Show();
+            }
+        }
+    }
+}
